4bc870d36525d2078b6405bdcafb6c126191be5b
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-05-20 08:09:16 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  //basePath: "/FMYDHUB",
+  basePath: "/FMYDHUB",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
